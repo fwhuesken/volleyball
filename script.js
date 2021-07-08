934acfa602dad1f73e9f6135edb2098cbe376083
Felix Huesken <fwhuesken@gmail.com>
2021-07-08 10:05:28 +0000

    Found a few bugs copying code

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const game = document.getElementById('game');
-const dropDowns = document.querySelectorAll('select.form-field_full');
+const dropDowns = document.querySelectorAll('select.form-field__full')
 const players = ["Laura", "Felix", "Martin", "Hanna", "Simon", "Nora", "Dennis", "David", "Giulia G.", "Giulia M.", "Jackie"];
 
 game.addEventListener("submit", logGame)
 
-//Populate dropdown menus
-dropDowns.forEach(dd => dd.innerHTML = players.map(player => `<option value>="${player}">${player}</option>)`).join(''));
 
-function logGame(e) {
-  e.preventDefault(); //cancel submit for now, later we can Ajax --> what does that mean?
+//Populate dropdown menus
+
+dropDowns.forEach(dd => dd.innerHTML = players.map(player => `<option value="${player}">${player}</option>`).join(''));
+
+
+function logGame() {
+ // e.preventDefault(); // cancel submit for now, later we can Ajax
   let winners = []
-  let loswers = []
-  dropDowns.forEach(dd, i) => {
+  let losers = []
+  dropDowns.forEach((dd, i) => {
     if (i <= 1) winners.push(dd.value)
     else losers.push(dd.value)
   })
+  console.log(winners, losers)
 }
